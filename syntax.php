ca77c5d93b038cbaa7ad56fd06fd04438c0dd3fa
madhunepal <madhunepal@gmail.com>
2020-06-14 23:24:54 -0400

    added basic syntax file

---
+++ syntax.php
@@ -0,0 +1,10 @@
+<?php
+echo "print my name, Madhu Nepal";
+?>
+<br>
+<?php
+//classes, functions, and user-defined functions are not case-sensitive.
+ECHO "print my name, Madhu Nepal";
+echo "print my name, Madhu Nepal";
+Echo "print my name, Madhu Nepal";
+?>
