2aa1e71c1a5545c6ba6a3d7265146ab383e35ef9
madhunepal <madhunepal@gmail.com>
2020-06-15 21:39:07 -0400

    loop folder is added with fuile

---
+++ loop/phploops.php
@@ -0,0 +1,19 @@
+<?php
+//php has four loops while, do .. while.. for .. foreach
+ //while loop syntax
+
+/*
+while (condition is true){
+    code will be executed;
+}
+*/
+
+$pen = 5;
+
+while($pen <= 10){
+
+    echo "the price is : $pen <br>";
+    $pen++;
+}
+
+?>
