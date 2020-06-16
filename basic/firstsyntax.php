3b11152647ab65929fa0541a3051461478ee9352
madhunepal <madhunepal@gmail.com>
2020-06-15 20:38:44 -0400

    comment is added

--- basic/firstsyntax.php
+++ basic/firstsyntax.php
@@ -1,6 +1,13 @@
 <?php
 
 echo "My name is Madhu Nepal, This is my fist code in php";
-
+//comment in first syntax of php 
+/* multiline
+comment
+looks like
+this
+where many lines 
+can be commented in single comment
+*/
 
 ?>
