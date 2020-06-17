3ab84702d6354c1883140e1ed766286e2b74bef6
madhunepal <madhunepal@gmail.com>
2020-06-16 21:03:24 -0400

    crud folder with files are added

---
+++ CRUD/config.php
@@ -0,0 +1,10 @@
+<?php
+//database related info
+$databaseHost = 'localhost';
+$databaseName = 'crud';
+$databaseUsername = 'root';
+$databasePassowrd = 'root';
+
+$mysqlink = mysqli_connect($databaseHost, $databaseUsername, $databasePassowrd, $databaseName);
+
+?>
